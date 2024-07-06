15a4df2eb07ef4f5ee4e0758ae394040402392b3
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-06 23:58:40 +0900

    fix: 신규사용자 -> db에 사용자 추가 로직 추가

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,29 +6,46 @@ exports.checkEmail = async (req, res) => {
   const email = req.body.email;
 
   try {
+    console.log("1");
     const userExists = await checkUserExistence(email);
+    console.log("6");
     if (userExists) {
       // 사용자 정보 가져오기
+
       const user = await User.findOne({ email });
       res.json({
         isExistingUser: true,
         nickname: user.nickname,
         profileImage: user.profileImage,
+        userId: user.userId,
       });
     } else {
+      console.log("7");
+      const newUser = new User(req.body);
+      console.log("8");
+      console.log(newUser);
+      await newUser.markModified("profileImage");
+      await newUser.save();
+      console.log("9");
       res.json({ isExistingUser: false });
+      console.log("10");
     }
   } catch (error) {
+    console.error("Error checking user existence:", error);
+
     res.status(500).json({ error: "Error checking user existence" });
   }
 };
 
 // 이미 존재하는 이메일인지 확인하는 함수
 const checkUserExistence = async (email) => {
+  console.log("2");
   try {
+    console.log("3");
     console.log(email);
-
+    console.log("4");
     const user = await User.findOne({ email });
+    console.log("5");
     return !!user; // 사용자가 존재하면 true, 그렇지 않으면 false 반환
   } catch (error) {
     console.error("데이터베이스 오류:", error);
--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  email: { type: String, unique: true },
   nickname: { type: String },
   profileImage: { type: String },
 });
