4bb30777552e39db056fae067b29b3ef6b5b5692
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-07 21:39:52 +0900

    feat: globalWord / personalWord 구분

--- routes/gameRouter.js
+++ routes/gameRouter.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const User = require("../models/User");
 const gameController = require("../controllers/gameController");
 
-router.get("/word", gameController.getRandomWord);
+router.get("/globalWord", gameController.getGlobalWord); // 모든 사용자가 동일한 단어를 받음
+router.get("/personalWord", gameController.getPersonalWord); // 각 사용자가 개별 단어를 받음
 
 module.exports = router;
