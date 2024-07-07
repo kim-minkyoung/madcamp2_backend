3aff57f4caf293e710e64a6b21e1ad7198b41e7f
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-07 21:20:49 +0900

    feat: 1분에 단어 하나 업데이트

--- routes/gameRouter.js
+++ routes/gameRouter.js
@@ -5,4 +5,6 @@ const router = express.Router();
 const User = require("../models/User");
 const gameController = require("../controllers/gameController");
 
+router.get("/word", gameController.getRandomWord);
+
 module.exports = router;
