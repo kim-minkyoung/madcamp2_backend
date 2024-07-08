45aa8b271e79c342c2b47efd457e161c22fd64ce
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-08 21:09:18 +0900

    fix: post chectEmail _id로 수정

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,7 @@ exports.checkEmail = async (req, res) => {
         isExistingUser: true,
         nickname: user.nickname,
         profileImage: user.profileImage,
-        userid: user._id,
+        _id: user._id,
       });
     } else {
       const newUser = new User(req.body);
