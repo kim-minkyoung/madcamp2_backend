a589850130c508f9e844af6d754c7d21af9ae37e
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-09 20:21:30 +0900

    feat: populate 사용

--- controllers/userInfoController.js
+++ controllers/userInfoController.js
@@ -14,11 +14,19 @@ exports.checkEmail = async (req, res) => {
         userInfo,
       });
     } else {
+      // 새 UserInfo 생성 및 저장
       const newUserInfo = new UserInfo(req.body);
       await newUserInfo.save();
+
+      // 새 User 생성 및 저장
       const newUser = new User({ userInfo: newUserInfo._id });
       await newUser.save();
-      res.json({ isExistingUser: false, user: newUser });
+
+      // 새 User를 userInfo 필드와 함께 반환
+      const populatedUser = await User.findById(newUser._id).populate(
+        "userInfo"
+      );
+      res.json({ isExistingUser: false, user: populatedUser });
     }
   } catch (error) {
     console.error("Error checking user existence:", error);
@@ -57,8 +65,15 @@ exports.updateNickname = async (req, res) => {
       return res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
     }
 
+    // 관련된 User 문서 업데이트
+    const updatedUser = await User.findOneAndUpdate(
+      { userInfo: userid },
+      { $set: { userInfo: updatedUserInfo._id } },
+      { new: true, runValidators: true }
+    ).populate("userInfo");
+
     // userInfo가 업데이트되면 관련 User도 업데이트 (후크를 통해 자동으로 이루어짐)
-    res.json(updatedUserInfo);
+    res.json({ userInfo: updatedUserInfo, user: updatedUser });
   } catch (error) {
     console.error("프로필 업데이트 오류:", error);
     res.status(500).json({ error: "프로필 업데이트 중 오류가 발생했습니다." });
