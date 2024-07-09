3f0d8d7e83ca0ab7530bcdd7f170e2b8e46d1a26
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-09 21:07:08 +0900

    url user

--- routes/userInfoRouter.js
+++ routes/userInfoRouter.js
@@ -4,16 +4,16 @@ const express = require("express");
 const router = express.Router();
 const userInfoController = require("../controllers/userInfoController");
 
-// 이메일 중복 여부 확인
+// 이메일 중복 여부 확인 (객체 전부다 return 하게 수정)
 router.post("/checkEmail", userInfoController.checkEmail);
 
 // 사용자 탈퇴
 router.delete("/:userid", userInfoController.deleteUser);
 
-// 사용자 정보 조회(프로필)
+// 사용자 정보 조회(프로필) (ok)
 router.get("/:userid", userInfoController.getProfile);
 
-// 닉네임 수정
+// 닉네임, 프사 수정 (ok)
 router.put("/:userid", userInfoController.updateProfile);
 
 module.exports = router;
