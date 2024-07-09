f672ae38654ac6c54f4344e876787ff0cbb614be
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-09 18:35:50 +0900

    feat: 회원가입 시 User  자동 생성

--- controllers/userInfoController.js
+++ controllers/userInfoController.js
@@ -1,6 +1,7 @@
 // controllers/userController.js
 
-const User = require("../models/UserInfo");
+const UserInfo = require("../models/UserInfo");
+const User = require("../models/User");
 
 exports.checkEmail = async (req, res) => {
   const email = req.body.email;
@@ -10,16 +11,15 @@ exports.checkEmail = async (req, res) => {
     if (userExists) {
       // 사용자 정보 가져오기
 
-      const user = await User.findOne({ email });
+      const user = await UserInfo.findOne({ email });
       res.json({
         isExistingUser: true,
-        nickname: user.nickname,
-        _id: user._id,
+        user: user,
       });
     } else {
       const newUser = new User(req.body);
       await newUser.save();
-      res.json({ isExistingUser: false });
+      res.json({ isExistingUser: false, user: newUser });
     }
   } catch (error) {
     console.error("Error checking user existence:", error);
@@ -31,7 +31,7 @@ exports.checkEmail = async (req, res) => {
 // 이미 존재하는 이메일인지 확인하는 함수
 const checkUserExistence = async (email) => {
   try {
-    const user = await User.findOne({ email });
+    const user = await UserInfo.findOne({ email });
     return !!user; // 사용자가 존재하면 true, 그렇지 않으면 false 반환
   } catch (error) {
     console.error("데이터베이스 오류:", error);
@@ -49,7 +49,7 @@ exports.updateNickname = async (req, res) => {
     }
 
     // 데이터베이스에서 사용자 업데이트
-    const updatedUser = await User.findByIdAndUpdate(
+    const updatedUser = await UserInfo.findByIdAndUpdate(
       userid,
       { nickname: nickname },
       { new: true, runValidators: true }
@@ -70,7 +70,7 @@ exports.deleteUser = async (req, res) => {
   const userid = req.params.userid;
 
   try {
-    const deletedUser = await User.findByIdAndDelete(userid);
+    const deletedUser = await UserInfo.findByIdAndDelete(userid);
 
     if (!deletedUser) {
       return res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
