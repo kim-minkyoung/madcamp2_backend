d5e637eb06331b1cd12dee2b98f09ccfa56d7b55
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-09 17:38:11 +0900

    fix: userRouter 방지

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,16 +1,16 @@
-// routes/userGameRouter.js
+// // routes/userGameRouter.js
 
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/userController");
+// const express = require("express");
+// const router = express.Router();
+// const userController = require("../controllers/userController");
 
-// 사용자 정보 조회(프로필)
-router.get("/:userid", userController.getProfile);
+// // 사용자 정보 조회(프로필)
+// router.get("/:userid", userController.getProfile);
 
-// 모든 사용자 점수를 내림차순으로 가져오기
-router.get("/", userController.getAllUsersSortedByScore);
+// // 모든 사용자 점수를 내림차순으로 가져오기
+// router.get("/", userController.getAllUsersSortedByScore);
 
-// PUT profile
-router.put("/:userid", userController.updateProfile);
+// // PUT profile
+// router.put("/:userid", userController.updateProfile);
 
-module.exports = router;
+// module.exports = router;
